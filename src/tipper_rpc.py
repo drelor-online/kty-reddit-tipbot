7395c3e30d48117117b3c6f8bebc8cf6ef056655
drelor-online <94981539+drelor-online@users.noreply.github.com>
2022-01-03 13:24:15 -0500

    Update tipper_rpc.py
    Updated for KTY

--- src/tipper_rpc.py
+++ src/tipper_rpc.py
@@ -1,19 +1,43 @@
 from datetime import datetime
-from stellar_sdk import Server, Account, Keypair, Network, TransactionBuilder, Asset
-from stellar_sdk.exceptions import SdkError
+from web3 import Web3
+import json
+import config
 
 from shared import (
-    DEFAULT_URL,
-    DEFAULT_FEE,
     CURRENCY,
-    CURRENCY_ISSUER,
+    CONTRACT;
     ACCOUNT,
-    SECRET,
+    PRIVATEKEY,
     LOGGER,
-    to_stroop,
-    from_stroop
 )
 
+bsc = "https://bsc-dataseed.binance.org/"
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+main_address= ACCOUNT
+contract_address = CONTRACT #be sure to use a BSC Address in uppercase format like this 0x9F0818B... 
+
+abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]')
+
+contract = web3.eth.contract(address=contract_address, abi=abi)
+
+
+me = '0x5977987.....'  #send from this address
+main_address= "0x9F....."   #to this address
+
+send = 1000
+amount = web3.toWei(send, 'ether')
+print(amount)
+
+nonce = web3.eth.getTransactionCount(me)
+print(nonce)
+
+token_tx = contract.functions.transfer(main_address, amount).buildTransaction({
+    'chainId':56, 'gas': 100000,'gasPrice': web3.toWei('10','gwei'), 'nonce':nonce
+})
+sign_txn = web3.eth.account.signTransaction(token_tx, private_key=YOURPRIVATEKEY)
+web3.eth.sendRawTransaction(sign_txn.rawTransaction)
+print(f"Transaction has been sent to {main_address}")
 server = Server(horizon_url=DEFAULT_URL)
 
 # Get a workable fee to prevent a transaction from getting stuck.
